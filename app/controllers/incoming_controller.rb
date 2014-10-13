32dad660a6ccf16b0268f4828390fc060b3bec14
Dave Guarino <dave@codeforamerica.org>
2014-10-13 15:50:28 -0700

    Add incoming messages view

---
+++ app/controllers/incoming_controller.rb
@@ -0,0 +1,12 @@
+class IncomingController < ApplicationController
+  def index
+    client = Twilio::REST::Client.new(ENV['TWILIO_BALANCE_PROD_SID'], ENV['TWILIO_BALANCE_PROD_AUTH'])
+    @phone_number_hash = Hash.new
+    client.account.incoming_phone_numbers.list.each do |number|
+      funnel_name = number.friendly_name
+      @phone_number_hash[number.phone_number] = funnel_name
+    end
+
+    @all_inbound_messages = Message.find_all_by_direction("inbound")
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ BalanceMetricsV2::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   root 'application#index'
+  get 'incoming' => 'incoming#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'

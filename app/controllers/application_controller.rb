ea273562eabd406a2466706afb27693722d4ed79
Jake Solomon <jacob@codeforamerica.org>
2014-10-15 09:58:17 -0700

    add basic metrics chart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,11 +12,45 @@ class ApplicationController < ActionController::Base
     end
 
     messages = Message.arel_table
-    @count_of_successful_messages = Message.where(messages[:body].matches("Hi! Your food stamp balance is%").or(messages[:body].matches("%El saldo de su cuenta%"))).count
+    @all_successful_messages = Message.where(messages[:body].matches("Hi! Your food stamp balance is%").or(messages[:body].matches("%El saldo de su cuenta%")))
+    @count_of_successful_messages = @all_successful_messages.count
     @successful_messages_by_week = Message.where(messages[:body].matches("Hi! Your food stamp balance is%").or(messages[:body].matches("%El saldo de su cuenta%"))).group_by_week(:date_sent).count
     @count_of_error_messages = Message.where(messages[:body].matches("I'm really sorry! We're having trouble contacting the EBT system right now.%")).count
     @error_messages_by_week = Message.where(messages[:body].matches("I'm really sorry! We're having trouble contacting the EBT system right now.%")).group_by_week(:date_sent).count
     @successful_messages_by_source = Message.where(messages[:body].matches("Hi! Your food stamp balance is %").or(messages[:body].matches("%El saldo de su cuenta%"))).group(:from_number).count
     @successful_messages_by_source = @successful_messages_by_source.map { |k,v| [@phone_number_hash[k],v] }.sort { |a,b| b[1] <=> a[1] }
+
+    # Charts
+    # all_successful_messages_data = view_context.create_daily_timeseries_from_messages(@all_successful_messages)
+    # data_total = {'name' => 'total',
+    #         'x' => all_successful_messages_data[0],
+    #         'y' => all_successful_messages_data[1]}
+    args = []
+    @phone_number_hash.keys.each do |source_number|
+      successful_messages_for_source = @all_successful_messages.where(messages[:from_number].eq(source_number))
+      if successful_messages_for_source.any?
+        data = view_context.create_daily_timeseries_from_messages(successful_messages_for_source)
+        data_hash = {'name' => @phone_number_hash[source_number],
+            'x' => data[0],
+            'y' => data[1]}
+        args << data_hash
+      end
+    end
+
+    kwargs={
+      "filename"=> "Balance Metrics",
+      "fileopt"=> "overwrite",
+      "style"=> {
+        "type"=> "scatter"
+        },
+        "layout"=> {
+          "title"=> "Balance Metrics",
+          "xaxis" => {"type" => "date"},
+          "xaxis" => {"name" => "# of successful balance checks"}
+        },
+        "world_readable"=> true
+      }
+
+    @plot_url = view_context.create_plot("plot", args, kwargs)
   end
 end
